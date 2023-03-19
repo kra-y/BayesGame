3a131e6b35caf458e843a823d9af236c67fbfa04
kra-y <keitharaneo@gmail.com>
2023-03-18 23:01:55 -0600

    resuming the bayes fishing game

--- FishBucket.R
+++ FishBucket.R
@@ -1,27 +1,45 @@
-#RScript to explain Bayes Posterior Probability and updating
+FishPopulation = sample(1:1000,1)
+prior = sample(1:FishPopulation,1)
 
-# So we have a fisherman and a lake with an undetermined number of fish in it.
+# Initialize a data frame to store the results of each trip
+bucket <- data.frame(
+  "day" = 1:21,
+  "n_caught" = c(sample(1:FishPopulation,1),rep(NA,20)),
+  "n_marked" = c(0,rep(NA,20)),
+  "hypoth" = c(prior,rep(NA,20)),
+  "posterior" = matrix(rep(NA,21*FishPopulation), ncol=FishPopulation)
+)
 
-# The fisherman casts a net and brings in say 20 fish, and marks all of them.
+# Iterate through each trip and update the posterior distribution
+for (i in 2:21) {
+  # Simulate catching some fish and marking them
+  n_caught = sample(1:FishPopulation,1)
+  n_marked = sample(1:n_caught,1)
+  
+  # Compute the likelihood of the data given each possible hypothesis
+  
+  
+  log_pmf <- function(k, n, p) {
+    lp <- lgamma(n + 1) - lgamma(k + 1) - lgamma(n - k + 1) + k * log(p) + (n - k) * log(1 - p)
+    return(lp)
+  }
+  
+  likelihood <- exp(log_pmf(n_marked, n_caught, 1:FishPopulation/1000))
+  
+  
+  
+  # Compute the evidence by summing the likelihoods over all possible hypotheses
+  evidence = sum(likelihood * dbeta(bucket$hypoth[1:i], 1, 1))
+  
+  # Compute the posterior distribution by applying Bayes' rule
+  posterior = likelihood * dgamma(bucket$hypoth[1:i], 1, 1) / evidence
+  
+  # Update the data frame with the new results
+  bucket$n_caught[i] = n_caught
+  bucket$n_marked[i] = n_marked
+  bucket$posterior[i,] = posterior
+  
+  # Sample a new hypothetical population size for the next iteration
+  bucket$hypoth[i+1] = sample(1:FishPopulation,1,prob=posterior)
+}
 
-# The next day the fisherman casts a net and reels is another 20 fish, 4 of which have marks from the previous day.
-
-
-# We need to assign the probabilities of the events.
-
-#P(H|E): is the probability that the number of fish in the lake (H) given the evidence. H is the value of the first guess in this case.
-
-
-
-
-N <- 100
-prior <- rep(1/N, N)
-evidence <- sum(prior * likelihood(1:N, 5, 40))
-posterior <- likelihood(1:N, 40, 40) * prior / evidence
-plot(posterior, type="h")
-
-N <- 1000
-prior <- rep(1/N, N)
-evidence <- sum(prior * likelihood(1:N, 40, 40))
-posterior <- likelihood(1:N, 40, 40) * prior / evidence
-plot(posterior, type="h")
