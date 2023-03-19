579ceaa89a0d7ffcfe64f44cef53987433fb8ba9
kra-y <keitharaneo@gmail.com>
2023-03-19 10:33:56 -0600

    starting the app h=now that we can populate a data set with fishing trips.

--- app.R
+++ app.R
@@ -1,10 +1,9 @@
+# Prior distribution: 
+# Start by visualizing the prior distribution of the number of fish in the lake.
+# You can use a histogram or a density plot to show the distribution.
+# Add sliders or input fields to allow users to adjust the parameters of the prior distribution, such as
+# the mean and standard deviation.
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
 #
 
 library(shiny)
@@ -13,16 +12,16 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Let's go fishin"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
+            sliderInput("Fish Population",
+                        "How many fish are actually in the lake:",
                         min = 1,
-                        max = 50,
-                        value = 30)
+                        max = 1000,
+                        value = 500)
         ),
 
         # Show a plot of the generated distribution
